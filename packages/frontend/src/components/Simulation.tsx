d2949adaacfa3b339a37aa5440abaaf497f315c4
bhargav <bhargav.annem@gmail.com>
2023-08-28 12:53:49 -0500

    threejs

--- packages/frontend/src/components/Simulation.tsx
+++ packages/frontend/src/components/Simulation.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect, useRef } from "react";
 import { WASMContext } from "../context/wasm";
 import { fragmentShaderSrc, vertexShaderSrc } from "@/webgl/shaders";
+import * as THREE from "three";
 
 const initShader = (
   gl: any,
@@ -36,69 +37,38 @@ export const RustFFM = () => {
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    if (!(canvas instanceof HTMLCanvasElement)) {
-      throw new Error("No html canvas element.");
+    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+
+    const fov = 75;
+    const aspect = canvas.height / canvas.width; // the canvas default
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
+
+    const scene = new THREE.Scene();
+
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 }); // greenish blue
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    function render(time: number) {
+      time *= 0.001; // convert time to seconds
+
+      cube.rotation.x = time;
+      cube.rotation.y = time;
+
+      renderer.render(scene, camera);
+
+      requestAnimationFrame(render);
     }
-
-    const gl = canvas.getContext("webgl2");
-
-    if (!gl) {
-      throw new Error("Unable to initialize WebGL.");
-    }
-    gl.clearColor(255, 255, 255, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    const vertexShader = initShader(gl, "VERTEX_SHADER", vertexShaderSrc);
-    const fragmentShader = initShader(gl, "FRAGMENT_SHADER", fragmentShaderSrc);
-
-    const program = gl.createProgram();
-
-    if (!program) {
-      throw new Error("Unable to create the program.");
-    }
-
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-
-    gl.linkProgram(program);
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-      throw new Error(
-        `Unable to link the shaders: ${gl.getProgramInfoLog(program)}`
-      );
-    }
-
-    gl.useProgram(program);
-
-    // UNIFORMS
-    const iResolutionLoc = gl.getUniformLocation(program, "iResolution");
-    gl.uniform2f(iResolutionLoc, canvas.width, canvas.height);
-
-    // position buffer
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array([-1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1]),
-      gl.DYNAMIC_DRAW
-    );
-
-    // index buffer
-
-    // create the buffer
-    const indexBuffer = gl.createBuffer();
-
-    // make this buffer the current 'ELEMENT_ARRAY_BUFFER'
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    const index = gl.getAttribLocation(program, "a_position");
-    const size = 2;
-    const type = gl.FLOAT;
-    const normalized = false;
-    const stride = 0;
-    const offset = 0;
-    gl.vertexAttribPointer(index, size, type, normalized, stride, offset);
-    gl.enableVertexAttribArray(index);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    requestAnimationFrame(render);
   }, []);
 
   return (
