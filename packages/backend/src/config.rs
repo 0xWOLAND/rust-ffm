60b4825d4e0eec14bc8af3163b498edc14b38426
bhargav <bhargav.annem@gmail.com>
2023-08-29 22:44:47 -0500

    update gravity constant

--- packages/backend/src/config.rs
+++ packages/backend/src/config.rs
@@ -1,5 +1,5 @@
 pub const MAX_DEPTH: f64 = 10.;
 pub const RESOLUTION: f64 = 1.;
-pub const G_CONSTANT: f64 = 6.67e-14;
+pub const G_CONSTANT: f64 = 6.67e-11;
 // astronomical unit
 pub const AU: f64 = 1e11;
