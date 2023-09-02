0cdc99a2cdd9150d93fe337d3bf9e85c3bb6ad50
bhargav <bhargav.annem@gmail.com>
2023-09-01 21:17:10 -0500

    gen

--- packages/backend/src/sgalic/mod.rs
+++ packages/backend/src/sgalic/mod.rs
@@ -2,5 +2,5 @@ pub mod bulge;
 pub mod cmf;
 pub mod config;
 pub mod disk;
+pub mod generate;
 pub mod halo;
-pub mod potential;
--- packages/backend/src/sgalic/potential.rs
+++
@@ -1,7 +0,0 @@
-use crate::octree::Grid;
-
-use super::config::Config;
-
-pub fn fill_potential_grid(coords_stars: Vec<(f64, f64, f64)>, config: Config) {
-    let grav_tree = Grid::new(1., (config.a_halo * 400) as f64);
-}
